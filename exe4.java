381440b521b42005de9a3f39b734208e2ad488a3
David Wesley Trentham <dtrentha@tesla27.eecs.utk.edu>
2017-10-23 10:02:32 -0400

    wrote more

--- exe4.java
+++ exe4.java
@@ -1,7 +1,8 @@
-import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.io.UnsupportedEncodingException;
+import java.lang.Math;
+import java.util.Objects;
 
 public class exe4 {
     
@@ -9,13 +10,12 @@ public class exe4 {
     
         String seed = "trenthamesworthy";
 
-        String result = sha2(4,seed);
+        findCycle(2,seed);
         
-        System.out.println(seed);
-        System.out.println(result);
+        //System.out.println(seed);
 
-        String next = sha2(4,"e505");
-        System.out.println(next);
+        //String next = sha2(4,"e505");
+        //System.out.println(next);
 
     }
     private static String getHex(byte[] hash) {
@@ -33,7 +33,9 @@ public class exe4 {
             //System.out.println(getHex(hash));
             md.update(hash);
             byte[] digest = md.digest();
-            return getHex(digest);
+			String hexHash = getHex(digest);
+			String nhash = hexHash.substring(0, Math.min(hexHash.length(), n));
+            return nhash;
         } catch(NoSuchAlgorithmException | UnsupportedEncodingException ex) {
             ex.printStackTrace();
             return ex.getMessage();
@@ -41,12 +43,44 @@ public class exe4 {
     
 	}
 
+	public static void findCycle(int n,String seed) {
+	
+		String tortoise1 = sha2(n,seed);
+		String hare = sha2(n,sha2(n,seed));
+		String tortoise2 = sha2(n,seed);
+		String last1 = null;
+		String last2 = null;
+		long count = 0;
 
+		while(Objects.equals(tortoise1, hare) != true) {
+			tortoise1 = sha2(n,tortoise1);
+			hare = sha2(n,sha2(n,hare));
+            count++;
+			if(count > 1000000000){
+				System.out.println("thats a lot");
+				return;
+			}
+			
+		}
+		
+		last1 = tortoise1;
 
+        while(Objects.equals(tortoise1,tortoise2) != true) {
+	
+			last1 = tortoise1;
+			last2 = tortoise2;
+			tortoise1 = sha2(n,tortoise1);
+			tortoise2 = sha2(n,tortoise2);
 
+		}
+		
+		System.out.println(tortoise1);
+		System.out.println(hare);
+		System.out.println(last1);
+		System.out.println(last2);
+		return;
 
-    
-
+	}
 
 
 
